0a93253443b006e26f1df0cf7fe2b2a90e5f5bec
kanyasrithong <ks4264@drexel.edu>
2025-02-17 15:14:44 -0500

    Create js zodiac objects

--- postload.js
+++ postload.js
@@ -1,6 +1,69 @@
-const birthday:;
-const birthday_month:;
-const birthday_day:;
+const birthday: ;
+const birthday_month: ;
+const birthday_day: ;
+
+const signs = [
+  {
+    name: 'Lewis',
+    image: 'images/stardew-lewis.png',
+    sound: ,
+  },
+  {
+    name: 'Marnie',
+    image: 'images/stardew-marnie.png',
+    sound: 'sounds/marnie-hay.mp3',
+  },
+  {
+    name: 'Robin',
+    image: 'images/stardew-robin.png',
+    sound: 'sounds/robin-thunk.mp3',
+  },
+  {
+    name: 'Demetrius',
+    image: 'images/stardew-demetrius.png',
+    sound: ,
+  },
+  {
+    name: 'Abigail',
+    image: 'images/stardew-abigail.png',
+    sound: 'sounds/abigail-flute.mp3',
+  },
+  {
+    name: 'Penny',
+    image: 'images/stardew-penny.png',
+    sound: 'sounds/penny-book.mp3',
+  },
+  {
+    name: 'Elliot',
+    image: 'images/stardew-elliot.png',
+    sound: 'sounds/elliot-ocean.wav',
+  },
+  {
+    name: 'Willy',
+    image: 'images/stardew-willy.png',
+    sound: 'sounds/willy-fish.mp3',
+  },
+  {
+    name: 'Emily',
+    image: 'images/stardew-emily.png',
+    sound: 'sounds/emily-parrot.mp3',
+  },
+  {
+    name: 'Junimos',
+    image: 'images/stardew-junimos.png',
+    sound: 'sounds/junimo-meep.mp3',
+  },
+  {
+    name: 'Wizard',
+    image: 'images/stardew-wizard.png',
+    sound: 'sounds/wizard-bubbles.mp3',
+  },
+  {
+    name: 'Krobus',
+    image: 'images/stardew-krobus.png',
+    sound: ,
+  }
+]
 
 let selected_sign = ;
 
