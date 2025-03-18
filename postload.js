c7ae75418b75d8368c772e0fd310f69b96a4a77e
kanyasrithong <ks4264@drexel.edu>
2025-03-18 12:37:21 -0400

    Fixed Wizard zodiac

--- postload.js
+++ postload.js
@@ -123,24 +123,9 @@ emily_obj.addEventListener('click', function() {zodiac_info('Emily')}, false);
 
 junimos_obj.addEventListener('click', function() {zodiac_info('The Junimos')}, false);
 
-wizard_obj.addEventListener('click', function() {zodiac_info('The Wizard')}, false);
+wizard_obj.addEventListener('click', function() {zodiac_info('Wizard')}, false);
 
 krobus_obj.addEventListener('click', function() {zodiac_info('Krobus')}, false);
-
-volume_obj.addEventListener('click', function(){
-  if (!volume_obj.classList.contains("unmuted")){
-    volume_obj.setAttribute('src', '/images/site-icons/stardew-unmuted.webp');
-    console.log('music on');
-    document.getElementById('theme').play();
-    volume_obj.classList.add('unmuted');
-  } else {
-    volume_obj.setAttribute('src', '/images/site-icons/stardew-muted.webp');
-    console.log('music off');
-    document.getElementById('theme').pause();
-    volume_obj.classList.remove('unmuted');
-  }
-}, false);
-
 
 function zodiac_info(which_one) {
   zodiac_name_obj.innerHTML = which_one;
@@ -149,7 +134,7 @@ function zodiac_info(which_one) {
       console.log('Lewis')
       zodiac_popup_obj.classList.remove('hide');
       zodiac_picture_obj.src = 'images/zodiac-profiles/lewis-profile.webp';
-      zodiac_sprites_obj.src = 'images/zodiac-sprites/lewis-sprites.webp'
+      zodiac_sprites_obj.src = 'images/zodiac-sprites/lewis-sprites.webp';
       zodiac_date_obj.innerHTML = 'December 22 - January 19';
       zodiac_description_obj.innerHTML = 'You are a natural born leader. Your duties are what motivate you, and you are loved by those around you.';
       zodiac_ld_obj.src = 'images/zodiac-ld/lewis-ld.webp';
@@ -160,7 +145,7 @@ function zodiac_info(which_one) {
       console.log('Marnie')
       zodiac_popup_obj.classList.remove('hide');
       zodiac_picture_obj.src = 'images/zodiac-profiles/marnie-profile.webp';
-      zodiac_sprites_obj.src = 'images/zodiac-sprites/marnie-sprites.webp'
+      zodiac_sprites_obj.src = 'images/zodiac-sprites/marnie-sprites.webp';
       zodiac_date_obj.innerHTML = 'January 20 - February 18';
       zodiac_description_obj.innerHTML = 'Optimism drives you, even in the most dire of circumstances. Adaptable and gentle, you are a animal lover at heart.';
       zodiac_ld_obj.src = 'images/zodiac-ld/marnie-ld.webp';
@@ -171,7 +156,7 @@ function zodiac_info(which_one) {
       console.log('Robin')
       zodiac_popup_obj.classList.remove('hide');
       zodiac_picture_obj.src = 'images/zodiac-profiles/robin-profile.webp';
-      zodiac_sprites_obj.src = 'images/zodiac-sprites/robin-sprites.webp'
+      zodiac_sprites_obj.src = 'images/zodiac-sprites/robin-sprites.webp';
       zodiac_date_obj.innerHTML = 'February 19 - March 20';
       zodiac_description_obj.innerHTML = 'You are hardworking and very dedicated to your craft. Somewhat reserved, you can become ruled by your emotions.';
       zodiac_ld_obj.src = 'images/zodiac-ld/robin-ld.webp';
@@ -182,7 +167,7 @@ function zodiac_info(which_one) {
       console.log('Demetrius')
       zodiac_popup_obj.classList.remove('hide');
       zodiac_picture_obj.src = 'images/zodiac-profiles/demetrius-profile.webp';
-      zodiac_sprites_obj.src = 'images/zodiac-sprites/demetrius-sprites.webp'
+      zodiac_sprites_obj.src = 'images/zodiac-sprites/demetrius-sprites.webp';
       zodiac_date_obj.innerHTML = 'March 21 - April 19';
       zodiac_description_obj.innerHTML = 'Consistent and reliable, you work well within scientific fields. Stubborn tendencies may rule you at times, but at your core you are very loyal and protective.';
       zodiac_ld_obj.src = 'images/zodiac-ld/demetrius-ld.webp';
@@ -193,7 +178,7 @@ function zodiac_info(which_one) {
       console.log('Abigail')
       zodiac_popup_obj.classList.remove('hide');
       zodiac_picture_obj.src = 'images/zodiac-profiles/abigail-profile.webp';
-      zodiac_sprites_obj.src = 'images/zodiac-sprites/abigail-sprites.webp'
+      zodiac_sprites_obj.src = 'images/zodiac-sprites/abigail-sprites.webp';
       zodiac_date_obj.innerHTML = 'April 20 - May 20';
       zodiac_description_obj.innerHTML = 'You refuse to let yourself succumb to those around you, a free-spirit who marches to the beat of their own drum. Your interests are varied, a true jack-of-all-trades.';
       zodiac_ld_obj.src = 'images/zodiac-ld/abigail-ld.webp';
@@ -204,7 +189,7 @@ function zodiac_info(which_one) {
         console.log('Penny')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/penny-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/penny-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/penny-sprites.webp';
         zodiac_date_obj.innerHTML = 'May 21 - June 21';
         zodiac_description_obj.innerHTML = 'You are patient, making you a natural fit for the role of teacher. A lover of books, you find beauty in the smallest details.';
         zodiac_ld_obj.src = 'images/zodiac-ld/penny-ld.webp';
@@ -215,7 +200,7 @@ function zodiac_info(which_one) {
         console.log('Elliot')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/elliot-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/elliot-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/elliot-sprites.webp';
         zodiac_date_obj.innerHTML = 'June 22 - July 22';
         zodiac_description_obj.innerHTML = 'A romantic, you embrace the simpler joys in life. You enjoy the soft melody of a piano and find pleasure in the written word.';
         zodiac_ld_obj.src = 'images/zodiac-ld/elliot-ld.webp';
@@ -226,7 +211,7 @@ function zodiac_info(which_one) {
         console.log('Willy')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/willy-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/willy-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/willy-sprites.webp';
         zodiac_date_obj.innerHTML = 'July 23 - August 22';
         zodiac_description_obj.innerHTML = 'You live to spread your interests with others, not for your own personal gain. Keeping to yourself most of the time, you tend to like doing things your way. ';
         zodiac_ld_obj.src = 'images/zodiac-ld/willy-ld.webp';
@@ -237,7 +222,7 @@ function zodiac_info(which_one) {
         console.log('Emily')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/emily-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/emily-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/emily-sprites.webp';
         zodiac_date_obj.innerHTML = 'August 23 - September 22';
         zodiac_description_obj.innerHTML = 'Outgoing and personable, your innate creativity rules your every move. You do well in social settings, but also love to delve into your personal hobbies.';
         zodiac_ld_obj.src = 'images/zodiac-ld/emily-ld.webp';
@@ -248,18 +233,18 @@ function zodiac_info(which_one) {
         console.log('Junimos')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/junimos-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/junimo-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/junimo-sprites.webp';
         zodiac_date_obj.innerHTML = 'September 23 - October 22';
         zodiac_description_obj.innerHTML = 'Curious and kind, you are known to often help those around you. You are ruled by a sense of justice, and generally work well within a team setting.';
         zodiac_ld_obj.src = 'images/zodiac-ld/junimos-ld.webp';
         //Play audio for this character
         play_sound('junimos-audio');
         break;
-    case 'The Wizard':
+    case 'Wizard':
         console.log('Wizard')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/wizard-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/wizard-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/wizard-sprites.webp';
         zodiac_date_obj.innerHTML = 'October 23 - November 21';
         zodiac_description_obj.innerHTML = 'Dark and mysterious, your unpredictable personality entices those around you. Intuitive and determined, you are always striving for greatness.';
         zodiac_ld_obj.src = 'images/zodiac-ld/wizard-ld.webp';
@@ -270,7 +255,7 @@ function zodiac_info(which_one) {
         console.log('Krobus')
         zodiac_popup_obj.classList.remove('hide');
         zodiac_picture_obj.src = 'images/zodiac-profiles/krobus-profile.webp';
-        zodiac_sprites_obj.src = 'images/zodiac-sprites/krobus-sprites.webp'
+        zodiac_sprites_obj.src = 'images/zodiac-sprites/krobus-sprites.webp';
         zodiac_date_obj.innerHTML = 'November 22 - December 21';
         zodiac_description_obj.innerHTML = 'Often misunderstood, you begin to warm up to those who get to know you. You have a philosophical, complex mind that thinks beyond the surface level.';
         zodiac_ld_obj.src = 'images/zodiac-ld/krobus-ld.webp';
@@ -280,12 +265,26 @@ function zodiac_info(which_one) {
   }
 }
 
+volume_obj.addEventListener('click', function(){
+  if (!volume_obj.classList.contains("unmuted")){
+    volume_obj.setAttribute('src', '/images/site-icons/stardew-unmuted.webp');
+    console.log('music on');
+    play_sound('theme');
+    volume_obj.classList.add('unmuted');
+  } else {
+    volume_obj.setAttribute('src', '/images/site-icons/stardew-muted.webp');
+    console.log('music off');
+    document.getElementById('theme').pause();
+    volume_obj.classList.remove('unmuted');
+  }
+}, false);
+
 //Play audio function
 const all_audios = document.querySelectorAll('audio');
 function play_sound(sound) {
   all_audios.forEach(this_audio => {
     this_audio.pause();
-    this_audio.currentTime = 0; //Reset sound file back to beginning
+    //Reset sound file back to beginning
   });
   document.getElementById(sound).play();
 }
